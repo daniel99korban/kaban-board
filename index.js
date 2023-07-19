e16c2711a2adec82e6c9752c56932412c3d2b78d
Daniel Lima Azevedo <620.406.143-70@dpema.br>
2023-07-19 13:51:30 -0300

    updates

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ let frames = [// let projects [] with nameProjects, in the next release
     {
         'id' : 0,
         'nameFrame' : 'Back-log',
-        'colorFrame' : '#ff0000',
+        'colorFrame' : '#C74844',
         'listTasks' : []
     },
     {
         'id' : 1,
         'nameFrame' : 'Doing',
-        // 'colorFrame' : '#0000ff',
+        'colorFrame' : '#2631AB',
         'listTasks' : []
     },
     {
         'id' : 2,
         'nameFrame' : 'Done',
-        // 'colorFrame' : '#00ff00',
+        'colorFrame' : '#5B9C35',
         'listTasks' : []
     },
 ]
@@ -23,18 +23,21 @@ let ulFrames = $('#list-frames');
 ulFrames.addClass("white");
 
 function builderFrame(name, color = '#000'){
-    // let cardContainer = 
-    let frame = $('<li>'),
-    divAdd = $('<div>').addClass('add_circle right'),
-    nameframe = $('<p>').text(name).addClass('card-panel white-text center');
-    nameframe.css("background-color", color);
-    frame.append(nameframe);
-    frame.append(divAdd);
-    frame.attr("class", 'col s4 m4 l4');
+    let titleContainer = $('<div>').addClass('row card-panel white-text'),
+        frame = $('<li>'),
+        addButton = $('<i>').text('add_circle').addClass('medium material-icons right col s4 m4 l4'),
+        nameframe = $('<p>').text(name).addClass('center col s8 m8 l8');
+
+    titleContainer.css("background-color", color);
+    titleContainer.append(nameframe);
+    titleContainer.append(addButton);
+    frame.attr("class", 'col s4 m4 l4 row');
+    frame.append(titleContainer);
     return frame;
 }
 
 frames.forEach(element => {
     let newFrame = builderFrame(element.nameFrame, element.colorFrame);
+    // ulFrames.addClass('padding-s');
     ulFrames.append(newFrame);
 });
